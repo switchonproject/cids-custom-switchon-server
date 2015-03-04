74d6b2134fa2bd4b5af227438f56f4d53ab09c12
Pascal Dihé <pascal@cismet.de>
2015-03-04 17:36:45 +0100

    fix for minor bug introduced in #7

--- src/main/java/de/cismet/cids/custom/switchon/search/server/PostFilterTagsSearch.java
+++ src/main/java/de/cismet/cids/custom/switchon/search/server/PostFilterTagsSearch.java
@@ -135,7 +135,7 @@ public final class PostFilterTagsSearch extends AbstractCidsServerSearch {
                             queryBuilder.append(" JOIN jt_resource_tag rjtrt ON rrr.id = rjtrt.resource_reference");
                             queryBuilder.append(" JOIN tag rtag ON rjtrt.tagid = rtag.id");
                             queryBuilder.append(" JOIN taggroup rtag_tg ON rtag.taggroup = rtag_tg.id");
-                            queryBuilder.append("WHERE TRUE AND");
+                            queryBuilder.append(" WHERE TRUE AND");
                             queryBuilder.append(" to_tsvector('english', rtag_tg.name) @@ to_tsquery('''")
                                     .append(tagGroup)
                                     .append("''')");
