f154885fea42b0ebd779c2a6a6d635df54e32c32
Pascal Dihé <pascal@cismet.de>
2016-02-05 16:50:05 +0100

    #51 changed tags search to return cids REST entities

--- src/main/java/de/cismet/cids/custom/switchon/search/server/TagsSearch.java
+++ src/main/java/de/cismet/cids/custom/switchon/search/server/TagsSearch.java
@@ -8,6 +8,7 @@
 package de.cismet.cids.custom.switchon.search.server;
 
 import Sirius.server.middleware.interfaces.domainserver.MetaService;
+import Sirius.server.middleware.types.MetaObject;
 
 import lombok.Getter;
 import lombok.Setter;
@@ -19,12 +20,10 @@ import org.openide.util.lookup.ServiceProvider;
 import java.rmi.RemoteException;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.LinkedList;
 import java.util.List;
-
-import de.cismet.cids.custom.switchon.search.server.types.Tag;
-import de.cismet.cids.custom.switchon.search.server.types.Taggroup;
 
 import de.cismet.cids.server.search.AbstractCidsServerSearch;
 import de.cismet.cids.server.search.SearchException;
@@ -46,7 +45,7 @@ public final class TagsSearch extends AbstractCidsServerSearch implements RestAp
 
     //~ Static fields/initializers ---------------------------------------------
 
-    private static final Logger LOG = Logger.getLogger(TagsSearch.class);
+    private static final Logger LOGGER = Logger.getLogger(TagsSearch.class);
     private static final String DOMAIN = "SWITCHON";
 
     public static final SearchInfo SEARCH_INFO;
@@ -77,10 +76,9 @@ public final class TagsSearch extends AbstractCidsServerSearch implements RestAp
 
         final SearchParameterInfo resultParameterInfo = new SearchParameterInfo();
         resultParameterInfo.setKey("return");
-        resultParameterInfo.setDescription("List<Tag> Collection");
+        resultParameterInfo.setDescription("Collection of Entities or empty collection");
         resultParameterInfo.setArray(true);
-        resultParameterInfo.setType(Type.JAVA_CLASS);
-        resultParameterInfo.setAdditionalTypeInfo("de.cismet.cids.custom.switchon.search.server.types.Tag");
+        resultParameterInfo.setType(Type.ENTITY);
         SEARCH_INFO.setResultDescription(resultParameterInfo);
     }
 
@@ -100,42 +98,40 @@ public final class TagsSearch extends AbstractCidsServerSearch implements RestAp
     //~ Methods ----------------------------------------------------------------
 
     @Override
-    public Collection<Tag> performServerSearch() throws SearchException {
+    public Collection<MetaObject> performServerSearch() throws SearchException {
         final String[] tagArray;
-        final LinkedList<Tag> tagList = new LinkedList<Tag>();
         final MetaService ms = (MetaService)getActiveLocalServers().get(DOMAIN);
         final StringBuilder queryBuilder = new StringBuilder();
 
         if ((this.taggroup == null) || this.taggroup.isEmpty()) {
             final String msg = "'taggroup' is a required parameter of this search function!";
-            LOG.error(msg);
+            LOGGER.error(msg);
             throw new SearchException(msg); // NOI18N
         }
 
         if ((this.tags != null) && !this.tags.isEmpty()) {
             tagArray = this.tags.split(",");
-            if (LOG.isDebugEnabled()) {
-                LOG.debug(tagArray.length + " tag found in '" + this.tags + "'");
+            if (LOGGER.isDebugEnabled()) {
+                LOGGER.debug(tagArray.length + " tag found in '" + this.tags + "'");
             }
         } else {
             tagArray = new String[0];
         }
 
-        LOG.info("searching for tags of taggroup '" + taggroup
+        LOGGER.info("searching for tags of taggroup '" + taggroup
                     + "' and restricting by " + tagArray.length + " tags");
 
         if (ms == null) {
             final String msg = "no metaservice for domain '" + DOMAIN + "' found!";
-            LOG.error(msg);
+            LOGGER.error(msg);
             throw new SearchException(msg); // NOI18N
         }
 
-        queryBuilder.append("SELECT DISTINCT tag.id AS tag_id, \n");
-        queryBuilder.append("       tag.name AS tag_name, \n");
-        queryBuilder.append("       tag.description AS tag_description, \n");
-        queryBuilder.append("       taggroup.id AS taggroup_id, \n");
-        queryBuilder.append("       taggroup.name AS taggroup_name, \n");
-        queryBuilder.append("       taggroup.description AS taggroup_description \n");
+        queryBuilder.append("SELECT DISTINCT "
+                    + "  (SELECT id\n"
+                    + "   FROM cs_class\n"
+                    + "   WHERE name ILIKE 'tag') as class_id, "
+                    + "   tag.id AS object_id, tag.name \n");
         queryBuilder.append("FROM tag \n");
         queryBuilder.append("JOIN taggroup ON tag.taggroup = taggroup.id \n");
         queryBuilder.append("WHERE taggroup.name = '").append(this.taggroup).append("' \n");
@@ -152,48 +148,28 @@ public final class TagsSearch extends AbstractCidsServerSearch implements RestAp
             queryBuilder.append(") \n");
         }
 
-        queryBuilder.append("ORDER BY taggroup.name, tag.name;");
+        queryBuilder.append("ORDER BY tag.name;");
 
         try {
-            if (LOG.isDebugEnabled()) {
-                LOG.debug(queryBuilder.toString());
+            if (LOGGER.isDebugEnabled()) {
+                LOGGER.debug(queryBuilder.toString());
             }
-            final ArrayList<ArrayList> resultset = ms.performCustomSearch(queryBuilder.toString()); // NOI18N
 
-            if (LOG.isDebugEnabled()) {
-                LOG.debug(resultset.size() + " different tags for " + tagArray.length
+            final MetaObject[] metaObjects = ms.getMetaObject(this.getUser(), queryBuilder.toString());
+            final ArrayList<MetaObject> collection = new ArrayList<MetaObject>(Arrays.asList(
+                        metaObjects));
+
+            if ((tagArray.length > 0) && (collection.size() != tagArray.length)) {
+                LOGGER.warn("expected to get " + tagArray.length + " tags but actually found "
+                            + collection.size() + " tags!");
+            }
+
+            if (LOGGER.isDebugEnabled()) {
+                LOGGER.info(collection.size() + " different tags for " + tagArray.length
                             + " taggroups in query result found");
             }
 
-            // add tagArray name + tagArray results count to the tagArray group list
-            Taggroup theTaggroup = null;
-            for (final ArrayList row : resultset) {
-                final int tagId = (int)row.get(0);
-                final String tagName = String.valueOf(row.get(1));
-                final String tagDescription = String.valueOf(row.get(2));
-
-                if (theTaggroup == null) {
-                    final int taggroupId = (int)row.get(3);
-                    final String taggroupName = String.valueOf(row.get(4));
-                    final String taggroupDescription = String.valueOf(row.get(5));
-
-                    theTaggroup = new Taggroup(taggroupId, taggroupName, taggroupDescription);
-                }
-
-                final Tag tag = new Tag(tagId, tagName, tagDescription, theTaggroup);
-
-                tagList.add(tag);
-            }
-
-            if ((tagArray.length > 0) && (tagList.size() != tagArray.length)) {
-                LOG.warn("expected to get " + tagArray.length + " tags but actually found "
-                            + tagList.size() + " tags!");
-            }
-
-            LOG.info("found " + tagList.size() + " for tags of taggroup '" + taggroup
-                        + "' restricted by " + tagArray.length + " tags");
-
-            return tagList;
+            return collection;
         } catch (final RemoteException re) {
             throw new SearchException("search for tags could not be performed", re); // NOI18N
         }
